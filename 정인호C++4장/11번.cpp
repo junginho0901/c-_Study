bb0da6d40a1e885ec31d2b04b9d2e0679e3915c6
junginho0901 <cn5114555@naver.com>
2022-07-08 13:45:47 +0900

    20220708c++

--- 정인호C++4장/11번.cpp
+++ 정인호C++4장/11번.cpp
@@ -1,72 +1,97 @@
-#include<iostream>
-using namespace std;
-
-class Container {
-	int size;
-public:
-	Container() { size = 10; }
-	void fill();
-	void consume();
-	int getSize();
-};
-void Container::fill() {
-	size = 10;
-}
-void Container::consume() {
-	size--;
-
-}
-int Container::getSize() {
-	return size;
-}
-class CoffeeVendingMachine {
-	Container tong[3];
-	void fill();
-	void selectEspresso();
-	void selectAmericano();
-	void selectSugarCoffee();
-	void show();
-public:
-	void run();
-};
-void CoffeeVendingMachine::run() {
-	cout << "***커피 자판기를 작동합니다." << endl;
-	while (1)
-	{
-
-	}
-}
-void CoffeeVendingMachine::fill() {
-	for (int i = 0; i < 3; i++)
-	{
-		tong[i].fill();
-	}
-}
-void CoffeeVendingMachine::selectAmericano() {
-	tong[0].consume();
-	tong[1].consume();
-	tong[1].consume();
-}
-void CoffeeVendingMachine::selectEspresso() {
-	tong[0].consume();
-	tong[1].consume();
-	
-}
-void CoffeeVendingMachine::selectSugarCoffee() {
-	tong[0].consume();
-	tong[1].consume();
-	tong[1].consume();
-	tong[2].consume();
-}
-void CoffeeVendingMachine::show() {
-	cout << "커피 남은 량:" << tong[0].getSize() << endl;
-	cout << "물 남은 량:" << tong[1].getSize() << endl;
-	cout << "설탕 남은 량:" << tong[2].getSize() << endl;
-}
-int main(void) {
-
-	CoffeeVendingMachine machine;
-	machine.run();
-
-	return 0;
-}
+//#include<iostream>
+//using namespace std;
+//
+//class Container {
+//	int size;
+//public:
+//	Container() { size = 10; }
+//	void fill();
+//	void consume();
+//	int getSize();
+//};
+//void Container::fill() {
+//	size = 10;
+//}
+//void Container::consume() {
+//	size--;
+//
+//}
+//int Container::getSize() {
+//	return size;
+//}
+//class CoffeeVendingMachine {
+//	Container tong[3];
+//	void fill();
+//	void selectEspresso();
+//	void selectAmericano();
+//	void selectSugarCoffee();
+//	void show();
+//public:
+//	void run();
+//};
+//void CoffeeVendingMachine::run() {
+//	cout << "***커피 자판기를 작동합니다." << endl;
+//	while (1)
+//	{
+//
+//		cout << "메뉴를 눌러주세요(1:에스프레소, 2:아메리카노, 3:설탕커피, 4:잔량보기, 5:채우기)>>";
+//		int num;
+//		cin >> num;
+//		switch (num)
+//		{
+//		case 1:
+//			selectEspresso();
+//			break;
+//		case 2:
+//			selectAmericano();
+//			break;
+//		case 3:
+//			selectSugarCoffee();
+//			break;
+//		case 4:
+//			show();
+//			break;
+//		case 5:
+//			fill();
+//			break;
+//		default:
+//			break;
+//		}
+//	}
+//}
+//void CoffeeVendingMachine::fill() {
+//	for (int i = 0; i < 3; i++)
+//	{
+//		tong[i].fill();
+//	}
+//}
+//void CoffeeVendingMachine::selectAmericano() {
+//	tong[0].consume();
+//	tong[1].consume();
+//	tong[1].consume();
+//	cout << "아메리카노를 드세요" << endl;
+//}
+//void CoffeeVendingMachine::selectEspresso() {
+//	tong[0].consume();
+//	tong[1].consume();
+//	cout << "에스프레소를 드세요" << endl;
+//}
+//void CoffeeVendingMachine::selectSugarCoffee() {
+//	tong[0].consume();
+//	tong[1].consume();
+//	tong[1].consume();
+//	tong[2].consume();
+//	cout << "설탕커피를 드세요" << endl;
+//}
+//void CoffeeVendingMachine::show() {
+//	cout << "커피 남은 량:" << tong[0].getSize() << endl;
+//	cout << "물 남은 량:" << tong[1].getSize() << endl;
+//	cout << "설탕 남은 량:" << tong[2].getSize() << endl;
+//}
+//int main(void) {
+//
+//	CoffeeVendingMachine machine;
+//	machine.run();
+//
+//	return 0;
+//}
