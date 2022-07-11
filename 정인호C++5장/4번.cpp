c7cbb0ecde9c44c6d9040b74a2ea2c56e2dcaf02
junginho0901 <cn5114555@naver.com>
2022-07-11 22:07:32 +0900

    20220711c++

---
+++ 정인호C++5장/4번.cpp
@@ -0,0 +1,32 @@
+//#include<iostream>
+//using namespace std;
+//bool bigger(int a, int b, int& big) {
+//	if (a == b)
+//		return true;
+//	else
+//	{
+//		if (a > b)
+//		{
+//			big = a;
+//		}
+//		else
+//			big = b;
+//
+//	}
+//		return false;
+//
+//}
+//
+//int main(void) {
+//	int a, b;
+//	int result;
+//	cin >> a;
+//	cin >> b;
+//
+//	if (bigger(a, b, result)) {
+//		cout << "같습니다." << endl;
+//	}
+//	else {
+//		cout << result << "가 더 큽니다.";
+//	}
+//}
