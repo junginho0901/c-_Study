9a346d1e2c612abbe7b018dac377bec5d8b0101d
junginho0901 <cn5114555@naver.com>
2022-07-13 21:22:43 +0900

    20220712c++

---
+++ 정인호C++6장/1번.cpp
@@ -0,0 +1,27 @@
+//#include<iostream>
+//using namespace std;
+//int add(int *a, int b, int *c) {
+//	int result = 0;
+//	for (int i = 0; i < b; i++)
+//	{
+//		result += a[i] + c[i];
+//	}
+//	return result;
+//}
+//int add(int *a, int b) {
+//	int result = 0;
+//	for (int i = 0; i < b; i++)
+//	{
+//		result += a[i];
+//	}
+//	return result;
+//}
+//
+//int main(void) {
+//	int a[] = { 1,2,3,4,5 };
+//	int b[] = { 6,7,8,9,10 };
+//	int c = add(a, 5);
+//	int d = add(a, 5, b);
+//	cout << c<<endl;
+//	cout << d << endl;
+//}
