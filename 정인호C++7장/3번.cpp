b86bec3a4ecf47cc0f72970fc72b4fb45802b589
junginho0901 <cn5114555@naver.com>
2022-07-18 00:25:27 +0900

    20220718c++

--- 정인호C++7장/3번.cpp
+++ 정인호C++7장/3번.cpp
@@ -1,70 +1,70 @@
-#include <iostream>
-#include <string>
-using namespace std;
-
-class Book
-{
-    string title;
-    int price, pages;
-
-public:
-    Book(string title = "", int price = 0, int pages = 0)
-    {
-        this->title = title;
-        this->price = price;
-        this->pages = pages;
-    }
-    void show()
-    {
-        cout << title << ' ' << price << "원 " << pages << " 페이지" << endl;
-    }
-    string getTitle()
-    {
-        return title;
-    }
-    Book &operator+=(int a);
-    Book &operator-=(int a);
-    bool operator==(int a)
-    {
-        if (price == a)
-            return true;
-        else
-            return false;
-    }
-    bool operator==(string a)
-    {
-        if (title == a)
-            return true;
-        else
-            return false;
-    }
-    bool operator==(Book b)
-    {
-        if (b.price == price && b.title == title)
-            return true;
-        else
-            return false;
-    }
-    bool operator!(){
-        if(price==0){
-            return true;
-        }
-        else return false;
-    }
-};
-
-Book &Book::operator+=(int a)
-{
-    price += a;
-    return *this;
-}
-Book &Book::operator-=(int a)
-{
-    this->price -= a;
-    return *this;
-}
-int main(void)
-{
-    Book book("벼룩시장" , 0 , 50); // 가격은 0 
-    if(!book) cout << "공짜다" << endl;
-}
+//#include <iostream>
+//#include <string>
+//using namespace std;
+//
+//class Book
+//{
+//    string title;
+//    int price, pages;
+//
+//public:
+//    Book(string title = "", int price = 0, int pages = 0)
+//    {
+//        this->title = title;
+//        this->price = price;
+//        this->pages = pages;
+//    }
+//    void show()
+//    {
+//        cout << title << ' ' << price << "원 " << pages << " 페이지" << endl;
+//    }
+//    string getTitle()
+//    {
+//        return title;
+//    }
+//    Book &operator+=(int a);
+//    Book &operator-=(int a);
+//    bool operator==(int a)
+//    {
+//        if (price == a)
+//            return true;
+//        else
+//            return false;
+//    }
+//    bool operator==(string a)
+//    {
+//        if (title == a)
+//            return true;
+//        else
+//            return false;
+//    }
+//    bool operator==(Book b)
+//    {
+//        if (b.price == price && b.title == title)
+//            return true;
+//        else
+//            return false;
+//    }
+//    bool operator!(){
+//        if(price==0){
+//            return true;
+//        }
+//        else return false;
+//    }
+//};
+//
+//Book &Book::operator+=(int a)
+//{
+//    price += a;
+//    return *this;
+//}
+//Book &Book::operator-=(int a)
+//{
+//    this->price -= a;
+//    return *this;
+//}
+//int main(void)
+//{
+//    Book book("벼룩시장" , 0 , 50); // 가격은 0 
+//    if(!book) cout << "공짜다" << endl;
+//}
--- 정인호C++7장/4번.cpp
+++ 정인호C++7장/4번.cpp
@@ -1,40 +1,40 @@
-#include <iostream>
-#include <string>
-using namespace std;
-class Book
-{
-    string title;
-    int price, pages;
-
-public:
-    Book(string title = "", int price = 0, int pages = 0)
-    {
-        this->title = title;
-        this->price = price;
-        this->pages = pages;
-    }
-    void show()
-    {
-        cout << title << ' ' << price << "원 " << pages << " 페이지" << endl;
-    }
-
-    string getTitle() { return title; }
-    friend bool operator<(Book book1, Book book2);
-}; // string b가 객체가 아님, 외부 함수 필요
-bool operator<(Book book1, Book book2)
-{
-    if (book1.title < book2.title)
-        return true;
-    else
-        return false;
-}
-
-int main(void)
-{
-    Book a("청춘", 2000, 300);
-    string b;
-    cout << "책 이름을 입력하세요>>";
-    getline(cin, b); // 키보드로부터 문자열로 책 이름을 입력 받음
-    if (b < a)
-        cout << a.getTitle() << "이 " << b << "보다 뒤에 있구나!" << endl;
-}
+//#include <iostream>
+//#include <string>
+//using namespace std;
+//class Book
+//{
+//    string title;
+//    int price, pages;
+//
+//public:
+//    Book(string title = "", int price = 0, int pages = 0)
+//    {
+//        this->title = title;
+//        this->price = price;
+//        this->pages = pages;
+//    }
+//    void show()
+//    {
+//        cout << title << ' ' << price << "원 " << pages << " 페이지" << endl;
+//    }
+//
+//    string getTitle() { return title; }
+//    friend bool operator<(Book book1, Book book2);
+//}; // string b가 객체가 아님, 외부 함수 필요
+//bool operator<(Book book1, Book book2)
+//{
+//    if (book1.title < book2.title)
+//        return true;
+//    else
+//        return false;
+//}
+//
+//int main(void)
+//{
+//    Book a("청춘", 2000, 300);
+//    string b;
+//    cout << "책 이름을 입력하세요>>";
+//    getline(cin, b); // 키보드로부터 문자열로 책 이름을 입력 받음
+//    if (b < a)
+//        cout << a.getTitle() << "이 " << b << "보다 뒤에 있구나!" << endl;
+//}
