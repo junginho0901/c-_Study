9adde8e16a6de2721f9ad4892c0c71255dad945b
junginho0901 <cn5114555@naver.com>
2022-08-21 00:46:35 +0900

    20220821c++

--- 정인호C++11장/정인호C++11장/5번.cpp
+++ 정인호C++11장/정인호C++11장/5번.cpp
@@ -1,17 +1,17 @@
-#include<iostream>
-#include<string>
-using namespace std;
-
-int main() {
-	string cmd;
-	cout << "cin.get(char*, int)로 문자열을 읽습니다." << endl;
-	while (true) {
-		cout << "종료하려면 exit를 입력하세요 >> ";
-		getline(cin, cmd); // 79개까지의 문자 읽음
-		if (cmd=="exit") {
-			cout << "프로그램을 종료합니다....";
-			return 0;
-		}
-		
-	}
-}
+//#include<iostream>
+//#include<string>
+//using namespace std;
+//
+//int main() {
+//	string cmd;
+//	cout << "cin.get(char*, int)로 문자열을 읽습니다." << endl;
+//	while (true) {
+//		cout << "종료하려면 exit를 입력하세요 >> ";
+//		getline(cin, cmd); // 79개까지의 문자 읽음
+//		if (cmd=="exit") {
+//			cout << "프로그램을 종료합니다....";
+//			return 0;
+//		}
+//		
+//	}
+//}
---
+++ 정인호C++11장/정인호C++11장/6장.cpp
@@ -0,0 +1,18 @@
+//#include<iostream>
+//#include<cmath>
+//#include<iomanip>
+//using namespace std;
+//int main() {
+//	cout << left << setw(15) << "Number";
+//	cout << left << setw(15) << "Square";
+//	cout << left << setw(15) <<"Square Root";
+//	cout << endl;
+//	for (int i = 0; i < 50; i = i + 5) {
+//		int square = i * i;
+//		double root = sqrt(i);
+//		cout << left << setw(15) << setfill('_') << i;
+//		cout << left << setw(15) << setfill('_') << square;
+//		cout << left << setw(15) << setfill('_') << root;
+//		cout << endl;
+//	}
+//}
